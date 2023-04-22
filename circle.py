851a44cc3e1364c3d04ca3d8bc636a1c1488a9bb
Wriki-tmsl <110682893+Wriki-tmsl@users.noreply.github.com>
2023-04-22 12:53:37 +0530

    Update circle.py

--- circle.py
+++ circle.py
@@ -6,7 +6,7 @@ def decdeg2dms(dd):
     mult = -1 if dd < 0 else 1
     mnt,sec = divmod(abs(dd)*3600, 60)
     deg,mnt = divmod(mnt, 60)
-    return int(mult*deg)%360, int(mult*mnt)%360, int(mult*sec)%360
+    return int(mult*deg)%360, int(mult*mnt)%60, int(mult*sec)%60
 n=int(input("Enter Degrees: "))
 o=int(input("Enter Minutes: "))
 q=int(input("Enter Seconds: "))
@@ -21,7 +21,7 @@ theta = [radians(s),radians(d),radians(b),radians(p),radians(c),radians(k)]
 beta = [decdeg2dms(s),decdeg2dms(d),decdeg2dms(b),decdeg2dms(p),decdeg2dms(c),decdeg2dms(k)]
 n=np.ones(6)
 ax = pl.subplot(111, polar=True)
-ax.scatter(theta,n)
+ax.scatter(theta,n,color='red')
 for i, txt in enumerate(beta):
     ax.annotate(txt, (theta[i], n[i]))
 pl.show()
